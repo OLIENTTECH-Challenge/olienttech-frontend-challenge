11f025229589ea02d9310f316413d8f1e26fab1d
Yuta Ohira <alesion90@gmail.com>
2023-11-18 16:59:51 +0900

    Add LinkCard component and reference section

---
+++ src/components/base/card/link-card.tsx
@@ -0,0 +1,39 @@
+import { classNames } from "@/libs/utils/clsx";
+import styles from "./link-card.module.css";
+
+type LinkCardProps = {
+  className?: string;
+  title: string;
+  description?: string;
+  href: string;
+  external?: boolean;
+};
+
+export const LinkCard = ({
+  className,
+  title,
+  description,
+  href,
+  external = false,
+}: LinkCardProps) => {
+  const externalProps = {
+    target: "_blank",
+    rel: "noopener noreferrer",
+  } satisfies React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >;
+
+  return (
+    <a
+      href={href}
+      className={classNames(styles.link, className)}
+      {...(external ? externalProps : {})}
+    >
+      <div className={styles.content}>
+        <h5>{title}</h5>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </a>
+  );
+};
--- src/components/domain/home/home-page.tsx
+++ src/components/domain/home/home-page.tsx
@@ -1,6 +1,44 @@
 import { LinkButton } from "@/components/base/button/link-button";
 import { HomeHeader } from "@/components/common/home-header/home-header";
 import styles from "./home-page.module.css";
+import { LinkCard } from "@/components/base/card/link-card";
+
+type Reference = {
+  title: string;
+  description: string;
+  href: string;
+  external?: boolean;
+};
+
+const references: Reference[] = [
+  {
+    title: "What is BCCD Design?",
+    description:
+      "BCCD Designはコンポーネントの意味と性質に基づいて体系的に分類する新しいソフトウェア設計手法です。",
+    href: "https://github.com/OLIENTTECH/gx-frontend-challenges/blob/main/docs/bccd-design.md",
+    external: true,
+  },
+  {
+    title: "Hoge?",
+    description: "hogehoge",
+    href: "/",
+  },
+  {
+    title: "Hoge?",
+    description: "hogehoge",
+    href: "/",
+  },
+  {
+    title: "Hoge?",
+    description: "hogehoge",
+    href: "/",
+  },
+  {
+    title: "Hoge?",
+    description: "hogehoge",
+    href: "/",
+  },
+];
 
 export const HomePage = () => {
   return (
@@ -26,10 +64,24 @@ export const HomePage = () => {
           <div className={styles.introStack}>
             <div className={styles.introStackButtons}>
               <LinkButton href="/">Get Started</LinkButton>
-              <LinkButton href="/" variant="outlined">
+              <LinkButton
+                href="https://github.com/OLIENTTECH/gx-frontend-challenges/blob/main/README.md"
+                variant="outlined"
+              >
                 README
               </LinkButton>
             </div>
+          </div>
+        </div>
+        <div className={styles.reference}>
+          <div className={styles.referenceHeading}>
+            <h3>Reference</h3>
+            <p>参考リンク集</p>
+          </div>
+          <div className={styles.referenceContents}>
+            {references.map((reference) => (
+              <LinkCard {...reference} />
+            ))}
           </div>
         </div>
       </main>
