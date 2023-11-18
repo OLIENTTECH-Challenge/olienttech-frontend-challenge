1b588ac57dd82bc1bded2ccb453bd3d2a8faf2b3
Yuta Ohira <alesion90@gmail.com>
2023-11-18 14:50:58 +0900

    setting alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@/": path.join(__dirname, "src/"),
+    },
+  },
   plugins: [react()],
-})
+});
