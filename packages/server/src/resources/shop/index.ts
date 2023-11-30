b7a703c797049e217c04125f90db22aa6c457928
Yuta Ohira <alesion90@gmail.com>
2023-11-30 17:03:43 +0900

    Add dummy shop data and API endpoints

--- packages/server/src/resources/shop/index.ts
+++ packages/server/src/resources/shop/index.ts
@@ -1,8 +1,25 @@
+import { Shop } from '@olienttech/model';
 import { Hono } from 'hono';
 
 const app = new Hono();
+
+const dummyShops: Shop[] = [
+  {
+    id: '1',
+    name: 'XXX薬局',
+  },
+  {
+    id: '2',
+    name: 'YYY薬局',
+  },
+];
+
 app.get('/', async (c) => {
-  return c.text('Hello Shop!');
+  return c.json({ data: dummyShops });
+});
+
+app.get('/:shopId', async (c) => {
+  return c.json({ data: dummyShops[0] });
 });
 
 export default app;
