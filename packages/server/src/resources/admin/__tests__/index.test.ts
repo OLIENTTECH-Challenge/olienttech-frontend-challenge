20752331dd304a902747c419c3c5d9c90c6c145c
Yuta Ohira <alesion90@gmail.com>
2023-11-30 18:21:02 +0900

    Add JWT utility functions and update admin resource

--- packages/server/src/resources/admin/__tests__/index.test.ts
+++ packages/server/src/resources/admin/__tests__/index.test.ts
@@ -1,21 +1,18 @@
 import { describe, expect, test } from '@jest/globals';
 import app from '..';
-import { verify } from 'hono/jwt';
-import { JWT_SECRET } from '@/libs/constants/env';
+import { verify } from '@/libs/utils/jwt';
 
 describe('API: /admin', () => {
   test('GET: /token', async () => {
     const res = await app.request('/token');
     const token = await res.text();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const decodedPayload = await verify(token, JWT_SECRET);
+    const decodedPayload = await verify(token);
 
     expect(res.status).toBe(200);
     expect(decodedPayload).toEqual({
       sub: 'user123',
       role: 'admin',
-      message: 'Hello Hono!',
     });
   });
 });
--- packages/server/src/resources/admin/index.ts
+++ packages/server/src/resources/admin/index.ts
@@ -1,16 +1,13 @@
-import { JWT_SECRET } from '@/libs/constants/env';
+import { sign } from '@/libs/utils/jwt';
 import { Hono } from 'hono';
-import { sign } from 'hono/jwt';
 
 const app = new Hono();
 
 app.get('/token', async (c) => {
-  const payload = {
+  const token = await sign({
     sub: 'user123',
     role: 'admin',
-    message: 'Hello Hono!',
-  };
-  const token = await sign(payload, JWT_SECRET);
+  });
   return c.text(token);
 });
 
