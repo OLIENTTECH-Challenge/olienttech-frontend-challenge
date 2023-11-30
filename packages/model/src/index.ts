cbdddb0b827d18648c1a638a80d86bd79eaf5eb6
Yuta Ohira <alesion90@gmail.com>
2023-11-30 16:59:23 +0900

    Add model package with initial files

---
+++ packages/model/src/index.ts
@@ -0,0 +1,40 @@
+/**
+ * 商品情報
+ */
+export type Product = {
+  /** 商品ID */
+  id: string;
+
+  /** 商品名 */
+  name: string;
+
+  description: string;
+
+  /** 商品カテゴリ */
+  categories: string[];
+
+  /** 商品画像 */
+  image: string;
+};
+
+/**
+ * 製造会社
+ */
+export type Manufacturer = {
+  /** メーカーID */
+  id: string;
+
+  /** メーカー名 */
+  name: string;
+};
+
+/**
+ * 販売会社
+ */
+export type Shop = {
+  /** 店舗ID */
+  id: string;
+
+  /** 店舗名 */
+  name: string;
+};
