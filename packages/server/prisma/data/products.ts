9d2b81286c846650c98bd404da0a316fc953ff82
Yuta Ohira <alesion90@gmail.com>
2023-12-01 22:39:03 +0900

    Add dist to .gitignore and update tsconfig.json

---
+++ packages/server/prisma/data/products.ts
@@ -0,0 +1,64 @@
+export const products = [
+  {
+    name: 'アレジオン',
+    description: '強力な抗ヒスタミン作用で鼻炎症状を和らげる薬',
+    categories: ['薬', '花粉症対策'],
+  },
+  {
+    name: 'リポビタンD',
+    description: 'ビタミンB群とタウリンを配合したエネルギードリンク',
+    categories: ['健康食品', 'エネルギードリンク'],
+  },
+  {
+    name: 'ハンドソープ',
+    description: '肌に優しく、効果的に手を清潔にするソープ',
+    categories: ['日用品', '衛生用品'],
+  },
+  { name: 'マスク', description: 'ウイルスや花粉から保護する使い捨てマスク', categories: ['日用品', '衛生用品'] },
+  {
+    name: 'バンドエイド',
+    description: '傷口を保護し、治癒を促進する使い捨て絆創膏',
+    categories: ['日用品', '医療用品'],
+  },
+  { name: 'アイボン', description: '目の疲れや充血を和らげる目薬', categories: ['薬', '目薬'] },
+  { name: 'ヴェポラップ', description: '風邪の症状を和らげる塗り薬', categories: ['薬', '風邪薬'] },
+  { name: 'ムヒ', description: 'かゆみを和らげる外用薬', categories: ['薬', '皮膚用薬'] },
+  { name: 'パブロン', description: '風邪の症状を緩和する内服薬', categories: ['薬', '風邪薬'] },
+  { name: 'ブルーレット', description: '口内炎やのどの痛みを和らげるスプレー', categories: ['薬', '口腔ケア'] },
+  { name: 'ヘパリーゼB', description: '肝機能を改善する薬', categories: ['薬', '健康食品'] },
+  { name: 'コンタック', description: '風邪の症状を一度に緩和する薬', categories: ['薬', '風邪薬'] },
+  { name: 'サロンパス', description: '筋肉痛や関節痛を和らげる貼り薬', categories: ['薬', '痛み止め'] },
+  { name: 'ベビーパウダー', description: '赤ちゃんの肌を保護するパウダー', categories: ['日用品', 'ベビーケア'] },
+  { name: 'ベビーオイル', description: '赤ちゃんの肌を保湿するオイル', categories: ['日用品', 'ベビーケア'] },
+  { name: 'ベビーソープ', description: '赤ちゃんの肌に優しいソープ', categories: ['日用品', 'ベビーケア'] },
+  {
+    name: 'ベビーローション',
+    description: '赤ちゃんの肌を保湿するローション',
+    categories: ['日用品', 'ベビーケア'],
+  },
+  {
+    name: 'ベビーシャンプー',
+    description: '赤ちゃんの髪と頭皮に優しいシャンプー',
+    categories: ['日用品', 'ベビーケア'],
+  },
+  {
+    name: 'ベビーワイプ',
+    description: '赤ちゃんのおむつ替え時に便利なウェットティッシュ',
+    categories: ['日用品', 'ベビーケア'],
+  },
+  {
+    name: 'ベビーミルク',
+    description: '栄養バランスを考えた赤ちゃん向けのミルク',
+    categories: ['食品', 'ベビーケア'],
+  },
+  {
+    name: 'ベビーフード',
+    description: '離乳食から食事への移行期に適したベビーフード',
+    categories: ['食品', 'ベビーケア'],
+  },
+  {
+    name: 'ベビービスケット',
+    description: '歯固めやおやつに適したベビービスケット',
+    categories: ['食品', 'ベビーケア'],
+  },
+];
