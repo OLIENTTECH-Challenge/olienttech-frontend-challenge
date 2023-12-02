d3fdfe6c8a97604fa4c0877b997f93f2c697f15f
Yuta Ohira <alesion90@gmail.com>
2023-12-02 10:05:14 +0900

    Remove eslint-disable comment in result.ts

--- packages/model/src/result.ts
+++ packages/model/src/result.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export type AppResponse<T = any> = SuccessResponse<T> | ErrorResponse;
