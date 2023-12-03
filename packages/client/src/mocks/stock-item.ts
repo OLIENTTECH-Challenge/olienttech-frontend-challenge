1c75f426268b8d7a06b1d7dd2e93397c3bc72d99
Suto <sutou1335michimasa@gmail.com>
2023-12-03 21:02:26 +0900

    Add @olienttech/model as a devDependency

--- packages/client/src/mocks/stock-item.ts
+++ packages/client/src/mocks/stock-item.ts
@@ -1,245 +1,276 @@
-import { Manufacturer } from '@/interfaces/stores';
+import { ManufacturerHandlingProduct } from '@olienttech/model';
 
-export const stockItem: Manufacturer[] = [
+export const handlingProducts: ManufacturerHandlingProduct[] = [
   {
-    id: 'S1001',
-    name: 'ココカラファイン',
-    stockItem: [
-      {
-        id: '1',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '2',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '3',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '4',
-        name: 'ユリノーム',
-        category: '利尿剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '5',
-        name: 'リポビタンD',
-        category: '栄養ドリンク',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '6',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '7',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '8',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '9',
-        name: 'ユリノーム',
-        category: '利尿剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '10',
-        name: 'リポビタンD',
-        category: '栄養ドリンク',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-    ],
+    id: 1,
+    stock: 100,
+    product: {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description 1',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
   },
   {
-    id: 'S1002',
-    name: 'ツルハドラッグ',
-    stockItem: [
-      {
-        id: '1',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '2',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '3',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '4',
-        name: 'ユリノーム',
-        category: '利尿剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '5',
-        name: 'リポビタンD',
-        category: '栄養ドリンク',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '6',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '7',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '8',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-    ],
+    id: 2,
+    stock: 200,
+    product: {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description 2',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
   },
   {
-    id: 'S1003',
-    name: 'サンドラッグ',
-    stockItem: [
-      {
-        id: '1',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '2',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '3',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '4',
-        name: 'ユリノーム',
-        category: '利尿剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '5',
-        name: 'リポビタンD',
-        category: '栄養ドリンク',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '6',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-    ],
+    id: 3,
+    stock: 300,
+    product: {
+      id: 3,
+      name: 'Product 3',
+      description: 'Description 3',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
   },
   {
-    id: 'S1004',
-    name: 'マツモトキヨシ',
-    stockItem: [
-      {
-        id: '1',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '2',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '3',
-        name: 'ロキソニン',
-        category: '鎮痛剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '4',
-        name: 'ユリノーム',
-        category: '利尿剤',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '5',
-        name: 'リポビタンD',
-        category: '栄養ドリンク',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '6',
-        name: 'アレグラFX',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-      {
-        id: '7',
-        name: 'アレジオン',
-        category: '抗アレルギー薬',
-        stock: 100,
-        imageURL: 'https://test-url',
-      },
-    ],
+    id: 4,
+    stock: 400,
+    product: {
+      id: 4,
+      name: 'Product 4',
+      description: 'Description 4',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+        {
+          id: 5,
+          name: 'Category 5',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
+  },
+  {
+    id: 5,
+    stock: 500,
+    product: {
+      id: 5,
+      name: 'Product 5',
+      description: 'Description 5',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+        {
+          id: 5,
+          name: 'Category 5',
+        },
+        {
+          id: 6,
+          name: 'Category 6',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
+  },
+  {
+    id: 6,
+    stock: 600,
+    product: {
+      id: 6,
+      name: 'Product 6',
+      description: 'Description 6',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+        {
+          id: 5,
+          name: 'Category 5',
+        },
+        {
+          id: 6,
+          name: 'Category 6',
+        },
+        {
+          id: 7,
+          name: 'Category 7',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
+  },
+  {
+    id: 7,
+    stock: 700,
+    product: {
+      id: 7,
+      name: 'Product 7',
+      description: 'Description 7',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+        {
+          id: 5,
+          name: 'Category 5',
+        },
+        {
+          id: 6,
+          name: 'Category 6',
+        },
+        {
+          id: 7,
+          name: 'Category 7',
+        },
+        {
+          id: 8,
+          name: 'Category 8',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
+  },
+  {
+    id: 8,
+    stock: 800,
+    product: {
+      id: 8,
+      name: 'Product 8',
+      description: 'Description 8',
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+        },
+        {
+          id: 3,
+          name: 'Category 3',
+        },
+        {
+          id: 4,
+          name: 'Category 4',
+        },
+        {
+          id: 5,
+          name: 'Category 5',
+        },
+        {
+          id: 6,
+          name: 'Category 6',
+        },
+        {
+          id: 7,
+          name: 'Category 7',
+        },
+        {
+          id: 8,
+          name: 'Category 8',
+        },
+        {
+          id: 9,
+          name: 'Category 9',
+        },
+      ],
+      image: 'https://images.unsplash.com/photo-1612837017391-5e9b9f4a4c9f',
+    },
   },
 ];
