446bcf3fcf6041ee49003c6e35f1b06fbca33a43
Suto <sutou1335michimasa@gmail.com>
2023-12-04 18:33:13 +0900

    Add pagination functionality to table component

--- packages/client/src/components/base/table/table.module.css.d.ts
+++ packages/client/src/components/base/table/table.module.css.d.ts
@@ -5,6 +5,9 @@ declare const styles:
   & Readonly<{ "table": string }>
   & Readonly<{ "table": string }>
   & Readonly<{ "table": string }>
+  & Readonly<{ "pagination": string }>
+  & Readonly<{ "pagination": string }>
+  & Readonly<{ "pagination": string }>
 ;
 export default styles;
 //# sourceMappingURL=./table.module.css.d.ts.map
--- packages/client/src/components/base/table/table.tsx
+++ packages/client/src/components/base/table/table.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './table.module.css';
 
 type TableColumn = {
@@ -7,31 +8,56 @@ type TableColumn = {
 
 type TableProps = {
   columns: TableColumn[];
-  //TODO: 型を決める
   data: any[];
 };
 
 const Table: React.FC<TableProps> = ({ columns, data }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const rowsPerPage = 10; // 1ページあたりの行数
+
+  const startIndex = currentPage * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const currentData = data.slice(startIndex, endIndex);
+
+  const pageCount = Math.ceil(data.length / rowsPerPage);
+
+  // ページ切り替え関数
+  const goToNextPage = () => { setCurrentPage((prev) => Math.min(prev + 1, pageCount - 1)); };
+  const goToPreviousPage = () => { setCurrentPage((prev) => Math.max(prev - 1, 0)); };
+
   return (
-    <div className={styles.container}>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            {columns.map((column, index) => (
-              <th key={index}>{column.header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {columns.map((column, columnIndex) => (
-                <td key={columnIndex}>{column.accessor(row)}</td>
+    <div>
+      <div className={styles.container}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column.header}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {currentData.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {columns.map((column, columnIndex) => (
+                  <td key={columnIndex}>{column.accessor(row)}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.pagination}>
+        <button onClick={goToPreviousPage} disabled={currentPage === 0}>
+          前へ
+        </button>
+        <span>
+          ページ {currentPage + 1} / {pageCount}
+        </span>
+        <button onClick={goToNextPage} disabled={currentPage === pageCount - 1}>
+          次へ
+        </button>
+      </div>
     </div>
   );
 };
