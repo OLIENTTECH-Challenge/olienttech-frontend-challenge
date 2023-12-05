aee790eccaf846dd0ca7d50394c5f664b32977c7
Suto <sutou1335michimasa@gmail.com>
2023-12-06 05:25:42 +0900

    Add mock manufacturers data

--- packages/client/src/components/domain/login/login.tsx
+++ packages/client/src/components/domain/login/login.tsx
@@ -2,18 +2,10 @@
 
 import { useState } from 'react';
 import { Selectbox } from '@/components/base/selectbox/selectbox';
-import { Manufacturer } from '@olienttech/model';
 import styles from './login.module.css';
 import { LinkButton } from '@/components/base/button/link-button/link-button';
 import { HomeHeader } from '@/components/common/home-header/home-header';
-
-//TODO: APIから取得、もしくは共通のファイルから取得
-export const manufacturers: Manufacturer[] = [
-  { id: 1, name: 'オリエント製薬', description: 'オリエント製薬は、東京都港区に本社を置く製薬会社である。' },
-  { id: 2, name: 'アレジオン製薬', description: 'アレジオン製薬は、東京都港区に本社を置く製薬会社である。' },
-  { id: 3, name: 'ハイハイ製薬', description: 'ハイハイ製薬は、東京都港区に本社を置く製薬会社である。' },
-  { id: 4, name: 'テクノ製薬', description: 'テクノ製薬は、東京都港区に本社を置く製薬会社である。' },
-];
+import { manufacturers } from '@/mocks/manufactures';
 
 export const LoginPage = () => {
   const [selectedManufacturerId, setSelectedManufacturerId] = useState<number | null>(manufacturers[0].id);
---
+++ packages/client/src/mocks/manufactures.ts
@@ -0,0 +1,8 @@
+import { Manufacturer } from '@olienttech/model';
+//TODO: APIから取得、もしくは共通のファイルから取得
+export const manufacturers: Manufacturer[] = [
+  { id: 1, name: 'オリエント製薬', description: 'オリエント製薬は、東京都港区に本社を置く製薬会社である。' },
+  { id: 2, name: 'アレジオン製薬', description: 'アレジオン製薬は、東京都港区に本社を置く製薬会社である。' },
+  { id: 3, name: 'ハイハイ製薬', description: 'ハイハイ製薬は、東京都港区に本社を置く製薬会社である。' },
+  { id: 4, name: 'テクノ製薬', description: 'テクノ製薬は、東京都港区に本社を置く製薬会社である。' },
+];
