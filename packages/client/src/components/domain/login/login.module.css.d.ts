5a0c48f1b2ab1aa228a2ee10e49070d5be6f1cdb
Suto <sutou1335michimasa@gmail.com>
2023-12-06 15:56:15 +0900

    Update CSS border-radius value

--- packages/client/src/components/domain/login/login.module.css.d.ts
+++ packages/client/src/components/domain/login/login.module.css.d.ts
@@ -1,6 +1,8 @@
-declare const styles: Readonly<{ main: string }> &
-  Readonly<{ card: string }> &
-  Readonly<{ title: string }> &
-  Readonly<{ spacer: string }>;
+declare const styles:
+  & Readonly<{ "main": string }>
+  & Readonly<{ "card": string }>
+  & Readonly<{ "title": string }>
+  & Readonly<{ "spacer": string }>
+;
 export default styles;
 //# sourceMappingURL=./login.module.css.d.ts.map
