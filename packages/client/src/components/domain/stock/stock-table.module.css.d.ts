e217cbeee263cea1ae69471ad253cafd45997e22
Suto <sutou1335michimasa@gmail.com>
2023-12-08 15:01:51 +0900

    Remove duplicate CSS class definitions in stock-table.module.css.d.ts

--- packages/client/src/components/domain/stock/stock-table.module.css.d.ts
+++ packages/client/src/components/domain/stock/stock-table.module.css.d.ts
@@ -1,12 +1,6 @@
 declare const styles:
   & Readonly<{ "container": string }>
   & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
   & Readonly<{ "icon": string }>
   & Readonly<{ "name": string }>
   & Readonly<{ "inputContainer": string }>
